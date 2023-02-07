3bea2cd5748674b131776f348f15af57478aee5f
Hubert <snailpiegeek@gmail.com>
2023-02-07 18:34:45 +0600

    Brands

--- src/com/company/car/classes/S/brands/models/Porsche_Cayenne.java
+++ src/com/company/car/classes/S/brands/models/Porsche_Cayenne.java
@@ -5,7 +5,7 @@ import com.company.details.engine.Engine;
 import com.company.details.transmission.Transmission;
 
 public class Porsche_Cayenne extends S_Porsche {
-    public Porsche_Cayenne(String brand, String model, Engine engine, Transmission transmission, double VIN, String color, int yearOfProduction, int price) {
-        super(brand, model, engine, transmission, VIN, color, yearOfProduction, price);
+    public Porsche_Cayenne(int id, Engine engine, Transmission transmission, double VIN, String color, int yearOfProduction, int price) {
+        super(id,"Cayenne", engine, transmission, VIN, color, yearOfProduction, price);
     }
 }
